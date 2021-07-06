00ef7af3a4c13a5fd8954afac87bc101dee87b76
jdlevish <45375460+jdlevish@users.noreply.github.com>
2021-07-06 12:47:17 -0700

    found another mistake in linking on deploy

--- src/pages/Salon.js
+++ src/pages/Salon.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../components/Nav.js';
 import Hero from '../components/hero';
-import SalonText from '../components/salonText.js';
+import SalonText from '../components/SalonText.js';
 import { Container } from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
 
